ff8a4332facceb91c639e3447090605f333ba280
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-09-11 13:07:47 +0530

    c

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
     cout<<"Gnanendra"<<endl;
-=======
     cout<<"gani"<<endl;
     cout<<"hello world"<<endl;
->>>>>>> 5b728d5bdaf71e216e72b708703a58bfd6d6c94f
     return 0;
 }
 
