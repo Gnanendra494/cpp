8a52107b620a22b675f8393c6a48ff9144136c26
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-09-14 00:20:59 +0530

    Create 6.cpp
    story

---
+++ 6.cpp
@@ -0,0 +1,28 @@
+// 1st method
+#include<iostream>
+using namespace std;
+int main()
+{
+  //string characterName = "Lucky";
+  //int characterAge = 21;
+  cout << "There was a man named Lucky" << endl;
+  cout << "He was 21 years old" << endl;
+  cout << "He liked the name Lucky" << endl;
+ cout << "But did not like being 21" << endl;
+return o;
+}
+// 2nd method
+//for long input of character name and age
+#include<iostream>
+using namespace std;
+int main()
+{
+  string characterName = "Lucky";
+  int characterAge = 21;
+cout << "Therre was a man named " << characterName << endl;
+cout << "He was " << characterAge << " years old " << endl;
+cout << "He liked the name " characterName << endl;
+  cout << "But did not like being " << characterAge << endl;
+
+return 0;
+}
