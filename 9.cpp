f7734d54348a9a5dcc9a95f7f2d36a20efdf5d58
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-09-14 01:23:46 +0530

    Create 9.cpp
    using math operations in cpp

---
+++ 9.cpp
@@ -0,0 +1,12 @@
+//
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  int wnum = 5;         // working with numbers
+  double mnum = 4.9;
+  cout << 10.0 / 3.3 << endl;
+  cout << pow(2 , 3  ) << endl;     //using math operations
+  return 0;
+}
