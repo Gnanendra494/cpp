5e948ff15e4fd6cd5861e1aeeda39c55be052835
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-09-14 01:03:20 +0530

    Create 7.cpp
    using data types in cpp

---
+++ 7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+
+  char grade = 'A';
+string pharse = "Learning Cpp";
+int age = 50;
+double gpa = 3.4;
+cout << "grade" << endl;
+  return 0;
+}
