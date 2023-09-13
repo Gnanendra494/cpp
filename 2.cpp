d21a2c0d7bd1bbd3ab25261f00ef3f88df89686b
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-09-13 14:26:48 +0530

    Swap 2 no's.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,2 +1,19 @@
+//Swapping two numbers
 #include<iostream>
-hi hlo git hub
+using namespace std;
+int main()
+{
+  int a,b;
+cout<<"Enter two numbers: ";
+cin>>a;
+cin>>b;
+cout<<"a= "<<a<<endl;
+cout<<"b= "<<b<<endl;
+int t=a;
+a=b;
+b=t;
+cout<<"After swapping"<<endl;
+cout<<"a= "<<a<<endl;
+cout<<"b= "<<b<<endl;
+return 0;
+}
