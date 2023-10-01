de66533233538704706337277ba3a5e70e5a940f
Gnanendra <124362831+Gnanendra494@users.noreply.github.com>
2023-10-01 13:35:30 +0530

    SumOf2no's.cpp
    Sum Of 2 No's in c++

---
+++ Sum2no's.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << " Input Two numbers:\n ";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << " Sum of "<< a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
